f69363f65537cab80a8e5123b1751e50521e0653
benyamintabarsi <benyamin.tabarsi@gmail.com>
2022-05-24 21:00:28 -0400

    Add src

--- src/snapclass/config/config.js
+++
@@ -1,5 +0,0 @@
-var consent_period = 2;
-// var new_consent_date = new Date("12 13, 2014 11:13:00")
-var useSnapCloud = true;
-var needConsent = false;
-var allowStudentChangePassword = true;
--- src/snapclass/src/assets/js/snap.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function loadSnap(project);
-
-export declare function saveSnap();
-
-export declare function sendMessage(message);
-
-export declare function newSnap();
